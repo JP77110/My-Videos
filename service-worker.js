67a5c140508fd96627fdf1c640e8d4b8c8108f8a
JP77110 <jptianthong@gmail.com>
2024-07-19 13:12:39 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response('Pas de connexion Internet', {
-        headers: { 'Content-Type': 'text/plain' }
-      });
-    })
-  );
-});
